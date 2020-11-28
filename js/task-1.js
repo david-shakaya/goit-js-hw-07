6465457d14c353a83a5ed7221ddd8a5cc03736b1
david-shakaya <devidoz666@gmail.com>
2020-11-28 19:05:32 +0200

    +++

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,10 @@
 console.log('Задача 1');
 
 function countElem(elem) {
-    
+
     const myTitle = document.querySelector(elem).children;
     console.log(`В списке  ${[...myTitle].length} категории.`)
-  
+
 }
 countElem('ul#categories') //ul#categories: В списке  3 категории.
 
@@ -23,4 +23,31 @@ findQuantityInList(0) //Категория: Животные
 findQuantityInList(1) //Категория: Продукты
             // Количество элементов: 3
 findQuantityInList(2) //Категория: Технологии
-            // Количество элементов: 5
+//             // Количество элементов: 5
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>Таже задача другой вариант решения
+
+// const ulRef = document.querySelectorAll('#categories >li');
+// console.log(`В списке ${ulRef.length}  категории.`);
+
+// const h2Ref = document.querySelectorAll('#categories >li > h2');
+// const liRef = document.querySelectorAll('#categories >li > ul');
+
+
+
+// let arr = []
+// const allCateg = h2Ref.forEach(element => {
+//   arr.push( element.textContent)
+// });
+
+
+// console.log(`Категория ${arr[0]}`);
+// console.log(`Количество елементов ${liRef[0].children.length} `);
+
+// console.log(`Категория ${arr[1]}`);
+// console.log(`Количество елементов ${liRef[1].children.length} `);
+
+// console.log(`Категория ${arr[2]}`);
+// console.log(`Количество елементов ${liRef[2].children.length} `);
+
--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,26 @@
+// const ingredients = [
+//   'Картошка',
+//   'Грибы',
+//   'Чеснок',
+//   'Помидоры',
+//   'Зелень',
+//   'Приправы',
+// ];
+
+// let arrElem = [];
+// const get = (arr) => arr.forEach(element => {
+
+//   const abbIngredients = document.createElement('li') // создаем лишки
+//    abbIngredients.textContent = element   // Добавляем в лишки текст. из масива ingredients[i] который проходим циклом
+//  arrElem.push(abbIngredients)
+  
+// })
+
+// const abbInDom = document.querySelector('#ingredients') //Находим елемент по ID 
+// get(ingredients)
+// abbInDom.append(...arrElem) 
+
+// >>>>>>>>>>>>>>>>
 const ingredients = [
   'Картошка',
   'Грибы',
@@ -7,16 +30,14 @@ const ingredients = [
   'Приправы',
 ];
 
-let arrElem = [];
-const get = (arr) => arr.forEach(element => {
+const ing = document.querySelector('#ingredients');
 
-  const abbIngredients = document.createElement('li') // создаем лишки
-   abbIngredients.textContent = element   // Добавляем в лишки текст. из масива ingredients[i] который проходим циклом
- arrElem.push(abbIngredients)
-  
-})
+const arr =[]
+ingredients.forEach(element => {
+  const heading = document.createElement('li');
+heading.textContent = element
+arr.push(heading)
+});
 
-const abbInDom = document.querySelector('#ingredients') //Находим елемент по ID 
-get(ingredients)
-abbInDom.append(...arrElem) 
-
+// console.log(...arr);
+ing.append(...arr) 
